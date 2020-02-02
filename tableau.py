994ec942759b527902c07c61e61ef2164143056d
Andre Pereira <andrelpoj@gmail.com>
2020-02-02 00:15:02 -0300

    Tableau - Conjunction Function

--- tableau.py
+++ tableau.py
@@ -1,20 +1,44 @@
-from typing import List
-
-def tableau(formulae: str, value: bool, true_vars: List[bool], false_vars: List[bool]):
-    if(len(formulae) == 0):
-        return (true_vars,false_vars)
+def tableau(formulae, value, true_vars, false_vars):
+    if(formulae.__contains__('&')):
+        return conjunction(formulae,value,true_vars, false_vars)
     elif(formulae.startswith('~')):
-        return negate(formulae.strip('~'), value, true_vars, false_vars)
+        return negate(formulae, value, true_vars, false_vars)
     else:
         return literal(formulae,value,true_vars,false_vars)
 
-def negate(formulae: str, value: bool, true_vars: List[bool], false_vars: List[bool]):    
-    return tableau(formulae, not value, true_vars, false_vars)
+def conjunction(formulae, value, true_vars, false_vars):
+    exp = formulae.split('&')
+    lhs = exp[0]
+    rhs = exp[1]
+    if value:
+        lhs_result = tableau(lhs, True, true_vars, false_vars)
+        if lhs_result[0]:
+            return tableau(rhs, True, lhs_result[1], lhs_result[2])
+        else: 
+            return (False,true_vars,false_vars)            
+    return 'whatever'
 
-def literal(literal: str, value: bool, true_vars: List[bool], false_vars: List[bool]):
-    true_vars.append(literal) if value else false_vars.append(literal)
-    
-    return(true_vars,false_vars)
+def negate(formulae, value, true_vars, false_vars):    
+    return tableau(formulae.strip('~'), not value, true_vars, false_vars)
 
-print(tableau("A",True, [],[]))
-print(tableau("~B",True, [],[]))
+def literal(literal, value, true_vars, false_vars):
+    return assign_value_check_contradiction(literal, value, true_vars, false_vars)
+
+def assign_value_check_contradiction(symbol, value, true_vars, false_vars):
+    if value:
+        if symbol in (false_vars):
+            return (False,true_vars,false_vars)
+        else:
+            updated = true_vars + tuple(symbol)
+            return(True,updated,false_vars)
+    else:
+        if symbol in (true_vars):
+            return (False,true_vars,false_vars)
+        else:
+            updated = false_vars + tuple(symbol)
+            return (True,true_vars,updated)
+
+print(tableau("A",True, (), ()))
+print(tableau("~B",True, (),()))
+print(tableau("A&B",True, (),()))
+print(tableau("A&~B",True, (),()))
