f52dad7356c31f14bf32ff42b13385e068e52db3
Andre Pereira <andrelpoj@gmail.com>
2020-02-26 05:44:15 -0300

    Stormpy GSPN model build

--- stormpy_test.py
+++ stormpy_test.py
@@ -1,17 +1,14 @@
 import stormpy 
 
 path = '/code/example_models/philosophers.jani'
-
-properties = { 
-    
-}
+formula = 'P=? [F eating1=1]'
 
 jani_program, properties = stormpy.parse_jani_model(path)
 
-print(properties)
+properties = stormpy.parse_properties_for_jani_model(formula,jani_program)
 
-#model = stormpy.build_model(jani_program, properties)
+model = stormpy.build_model(jani_program, properties)
 
-#print(model.model_type)
+print(model.model_type)
 
-#print("Labels: {}".format(model.labeling.get_labels()))
+print("Labels: {}".format(model.labeling.get_labels()))
